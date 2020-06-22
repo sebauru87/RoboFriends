82600553638ce1a162ff6893e9aada4f14d59270
Sebastian <Ferreira>
2020-06-22 11:45:50 -0300

    add json url. Add componentDidMount. Add if statment for loading

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import SearchBox from "./SearchBox";
 import CardList from "./CardList";
-import {robots} from './robots';
-import './App.css';
-
+// import {robots} from './robots';
+import "./App.css";
 
 class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: robots,
-            searchfield: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
 
-    onSearchChange=(event)=>{
-        this.setState({searchfield: event.target.value});
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
 
-    }
-
-    render() {
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-        //console.log(filteredRobots);
-    return (
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    //console.log(filteredRobots);
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
         <div className="tc">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={filteredRobots}/>
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
         </div>
-    );
+      );
+    }
+  }
 }
-}
-
 
 export default App;
